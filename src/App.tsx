445d511fe047c3c806a72b2ca0e3c15794524fde
Igor Tsuker <igorek888rane@gmail.com>
2023-02-12 20:55:16 +0300

    useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { useTheme } from './hook/useTheme'
+
 function App() {
-	return <div className='App'>Hello</div>
+	const { setTheme } = useTheme()
+	return (
+		<div className='App'>
+			<div className={'content'}>
+				<h1>Theme</h1>
+				<div style={{ display: 'flex' }}>
+					<div
+						onClick={() => setTheme('light')}
+						style={{ padding: '5px', cursor: 'pointer', border: '1px solid  ' }}
+					>
+						Light solid
+					</div>
+					<div
+						onClick={() => setTheme('dark')}
+						style={{ padding: '5px', cursor: 'pointer', border: '1px solid ' }}
+					>
+						Dark
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App
---
+++ src/hook/useTheme.ts
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react'
+
+export const useTheme = () => {
+	const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light')
+	useEffect(() => {
+		document.documentElement.setAttribute('data-theme', theme)
+		localStorage.setItem('theme', theme)
+	}, [theme])
+	return { setTheme }
+}
